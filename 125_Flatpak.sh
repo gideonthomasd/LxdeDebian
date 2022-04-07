e145ed79f9a11e64d64a0e3ccb77df095d0fc3aa
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-07 14:33:09 +0100

    Add files via upload

--- 125_Flatpak.sh
+++ 125_Flatpak.sh
@@ -6,3 +6,5 @@ sudo apt install flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.github.unrud.VideoDownloader
 flatpak install flathub org.xiphos.Xiphos
+
+sudo apt install -y snapd
