145d2cdc4cf08ab1d1c145f92add21e04bf78844
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-07 18:49:19 +0100

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+nm-applet &
+parcellite &
+#blueman-applet &
+#volumeicon &
+lxpolkit &
+#slstatus &
+dwmblocks &
+picom &   # Needed for dwm
+sxhkd -c ~/Luke/sxhkdrc &
+####~/.config/bspwm/testwallpaper &
