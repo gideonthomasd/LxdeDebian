5bac1084221c69a9a41ec1fee6274d8f925ae718
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-07 23:42:48 +0100

    Update 200_yt-dlp.sh

--- 200_yt-dlp.sh
+++ 200_yt-dlp.sh
@@ -1,4 +1,4 @@
 sudo snap install yt-dlp
-getsnap
+cp /var/lib/snapd/desktop/applications/*.desktop ~/.local/share/applications/
 #alias yt7='yt-dlp -o "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -f "best[height>=720][ext=mp4]"'
 #alias yta='yt-dlp -ic --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0'
