7fa31767225ea7022565220a5ab957aae84dc61a
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-08 09:10:28 +0100

    Update 160-After-Reboot-Python.sh

--- 160-After-Reboot-Python.sh
+++ 160-After-Reboot-Python.sh
@@ -3,5 +3,5 @@
 sudo snap install pycharm-community --edge --classic
 #sudo snap install pycharm-community --classic
 sudo apt install -y python3-tk
-sudo apt install -y sqlite3
+sudo apt install -y sqlite3   # pip install pysqlite3
 cp /var/lib/snapd/desktop/applications/*.desktop ~/.local/share/applications/
