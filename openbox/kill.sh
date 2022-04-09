1642ec593f465572757ae4d68355ce9a21f64bb5
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-09 08:42:19 +0100

    Add files via upload

---
+++ openbox/kill.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sleep 15s
+
+killall mintUpdate
+killall mintreport-tray
+killall applet.py
+killall blueberry-obex-agent
+killall obexd
+killall tumblerd
+killall xdg-permission-store
+killall xdg-documents-portal
+killall xdg-desktop-portal
+killall tracker-miner-fs
