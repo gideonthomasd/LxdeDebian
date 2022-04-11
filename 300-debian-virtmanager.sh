45711cd15eafef0af26d2073a5e15cb8a48c1777
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-11 17:28:03 +0100

    Add files via upload

---
+++ 300-debian-virtmanager.sh
@@ -0,0 +1,9 @@
+###  **Install QEMU-KVM on Debian 11**
+sudo apt install -y gnome-boxes virt-manager ebtables dnsmasq nftables qemu qemu-kvm qemu-system-x86 ovmf seabios libvirt-daemon-system libvirt-daemon-system-systemd 
+sudo apt install -y libvirt-daemon-driver-qemu libvirt-daemon libvirt-clients bridge-utils  
+
+sudo usermod -a -G kvm phil
+sudo usermod -a -G libvirt phil
+
+sudo systemctl enable libvirtd
+sudo systemctl start libvirtd
