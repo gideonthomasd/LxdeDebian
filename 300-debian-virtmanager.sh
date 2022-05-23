de5a19acff8435bd853051e05d0cd0588c910b76
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-05-23 23:35:38 +0100

    Update 300-debian-virtmanager.sh

--- 300-debian-virtmanager.sh
+++ 300-debian-virtmanager.sh
@@ -1,5 +1,12 @@
 ###  **Install QEMU-KVM on Debian 11**
 #!/bin/bash
+
+######################################################################################################
+##### Can use mxlinux to install this system (or virtualbox) #########################################
+######################################################################################################
+
+############################ OR Do the code below ####################################################
+
 sudo apt install -y gnome-boxes virt-manager ebtables dnsmasq nftables qemu qemu-kvm qemu-system-x86 ovmf seabios libvirt-daemon-system libvirt-daemon-system-systemd 
 sudo apt install -y libvirt-daemon-driver-qemu libvirt-daemon libvirt-clients bridge-utils  
 
